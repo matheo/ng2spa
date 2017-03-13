7a7f863bd1fdfe275394e4dfb54a209c06d765d2
Mateo TibaPalacios <nestormateo@gmail.com>
2017-03-13 12:08:29 -0500

    add(pipe): firstWord pipe to extract the day of the fullDate

---
+++ src/app/first-word.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { FirstWordPipe } from './first-word.pipe';
+
+describe('FirstWordPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FirstWordPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
